1b49c264f80c52f6bab332c67bc0e9c06692b079
Josh Thompson <thompsonjoshd@gmail.com>
2017-05-17 11:11:28 -0600

    not hardcoding zip anymore

--- alt-fuel-finder/app/controllers/search_controller.rb
+++ alt-fuel-finder/app/controllers/search_controller.rb
@@ -3,6 +3,6 @@ class SearchController < ApplicationController
 
   def index
     conn = NrelService.new
-    @stations = conn.nearest_stations_by_zip(80203)[0..9]
+    @stations = conn.nearest_stations_by_zip(params["q"])[0..9]
   end
 end
