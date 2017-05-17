a5998c4f4d751517b87fe12f79185421e5cd0f26
Josh Thompson <thompsonjoshd@gmail.com>
2017-05-17 10:27:11 -0600

    formatted list of stations making it through to view

--- alt-fuel-finder/app/controllers/search_controller.rb
+++ alt-fuel-finder/app/controllers/search_controller.rb
@@ -4,9 +4,8 @@ class SearchController < ApplicationController
   def index
     conn = Faraday.new("https://developer.nrel.gov")
     raw = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=S9nWjdAxKadPGBVKfBnjdIRaemILUjbVKpY5M6Zi&location=80203&radius=6&fuel_type=ELEC,LPG")
-    pretty = raw.body
-    parse(pretty)
-    # @stations = pretty["fuel_stations"]
+    pretty = JSON.parse(raw.body, symbolize_names: true)
+    @stations = pretty[:fuel_stations]
   end
 
 
