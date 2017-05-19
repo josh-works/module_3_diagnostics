84896f0e6d27ee46a1a233b931bd34dfe010f875
Josh Thompson <thompsonjoshd@gmail.com>
2017-05-18 22:24:46 -0600

    using Struct objects in view
    all tests pass
    
    I'm not using OpenStruct, just Struct
    the views are now calling methods on the station objects,
    instead of reading straight from the hashes
    I think this is a much cleaner approach.

--- alt-fuel-finder/app/models/station.rb
+++ alt-fuel-finder/app/models/station.rb
@@ -1,11 +1,14 @@
 class Station
   attr_reader :service
   Station = Struct.new("Station",
+                      :id,
                       :fuel_type_code,
                       :station_phone,
                       :city,
                       :intersection_directions,
                       :street_address,
+                      :fuel_type_code,
+                      :access_days_time,
                       :distance
                       )
 
@@ -16,11 +19,14 @@ class Station
   def self.nearest_stations_by_zip(zip_code, station_count = 10)
     service.nearest_stations_by_zip(zip_code).map do |station|
       Station.new(
+                  station[:id],
                   station[:fuel_type_code],
                   station[:station_phone],
                   station[:city],
                   station[:intersection_directions],
                   station[:street_address],
+                  station[:fuel_type_code],
+                  station[:access_days_time],
                   station[:distance]
                   )
     end
